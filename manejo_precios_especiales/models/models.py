b8f1987a04a130e890d05145587c7d9805a215fa
luis <angelchicles97@gmail.com>
2019-08-09 10:28:56 -0500

    modificacion campos y reporte

--- manejo_precios_especiales/models/models.py
+++ manejo_precios_especiales/models/models.py
@@ -4,19 +4,6 @@ from odoo import models, fields, api
 from openerp.exceptions import UserError, RedirectWarning, ValidationError
 
 
-# class add_sales(models.Model):
-#     _name = 'add_sales.add_sales'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
-#         
-
 class add_campos(models.Model):
     _inherit = 'sale.order.line'  
 
@@ -24,15 +11,13 @@ class add_campos(models.Model):
     public_price = fields.Float(string="Precio Publico" , related='product_id.lst_price', default=0.0, readonly=True,)
     especial_price= fields.Float(string="Precio especial", compute="_compute_amount2")
 
-
-    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id','order_id')
     def _compute_amount2(self):
-        """
-        Compute the amounts of the SO line.
-        """
+
         for line in self:
             if line.discount !=0.00:
                 if line.discount <=20:
+                    line.order_id.dis =True
                     price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
                     taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
                     line.update({
@@ -41,4 +26,11 @@ class add_campos(models.Model):
                         'especial_price': taxes['total_excluded'],
                     })
                 else:
-                    raise ValidationError('Porcentaje es mayor a lo permitido.') 
+                    raise ValidationError('Porcentaje es mayor a lo permitido.')
+            else:
+                line.order_id.dis=False       
+
+class campo_tru(models.Model):
+        _inherit = 'sale.order'  
+        
+        dis = fields.Boolean(string="true")
